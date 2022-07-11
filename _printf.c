5d75c3bd61571edfdbe8d62ea41911318ee58f71
Getinet <getinetay@gmail.com>
2022-07-11 03:08:43 -0700

    print formatted string

--- _printf.c
+++ _printf.c
@@ -31,6 +31,7 @@ int _printf(const char *format, ...)
 
 	va_start(arg_list, format);
 	/*Calling parser function*/
+	
 	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
 	return (printed_chars);
